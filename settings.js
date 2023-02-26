60e6764bb783070b18661673402b58461616682e
AstrooBoyy <101609744+AstrooBoyy@users.noreply.github.com>
2023-02-26 14:21:31 +0200

    upload project

---
+++ settings.js
@@ -0,0 +1,7 @@
+const ASTAR_MAINNET_ID = 592
+const SHIDEN_MAINNET_ID = 336 
+const SHIDEN_TESTNET_ID = 81
+
+const ASTAR_MAINNET_RPC = 'https://astar.api.onfinality.io/public'
+const SHIDEN_MAINNET_RPC = 'https://shiden.blastapi.io/e57947c1-4eea-4084-b0b7-dae396aeae82'
+const SHIDEN_TESTNET_RPC = 'https://shibuya.blastapi.io/e57947c1-4eea-4084-b0b7-dae396aeae82'
